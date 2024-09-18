d064ab59757d5639288e7e971e7e0a7dc37c1186
arnasir-amcc <arnasir.amcc@outlook.com>
2024-09-18 01:32:36 +0100

    LW-6 #done Much of the overlay design is done, remaining is the button functionality and actual transition upwards.

--- script.js
+++ script.js
@@ -13,4 +13,35 @@ function injectFooter() {
     document.body.appendChild(footer);
 }
 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('overlay-button').addEventListener('click', function() {
+        document.getElementById('landing-page-overlay').classList.add('overlay-remove');
+        console.log("Overlay removed.");
+    });
+});
+
+const overlayBoxes = document.querySelectorAll('.overlay-centre-boxes');
+const landingOverlay = document.getElementById('landing-page-overlay');
+const overlayNameBackground = document.getElementById('overlay-name-background');
+
+// Loop through each .overlay-centre-boxes to add the hover events
+overlayBoxes.forEach(box => {
+  // Add event listener for hovering (mouse over)
+  box.addEventListener('mouseover', function() {
+    overlayNameBackground.style.opacity = '0.8';
+    landingOverlay.style.opacity = '0.5'; // Change the opacity of the parent
+  });
+
+  // Add event listener for when the mouse leaves (mouse out)
+  box.addEventListener('mouseout', function() {
+    overlayNameBackground.style.opacity = '0.65';
+    landingOverlay.style.opacity = '1'; // Reset the opacity of the parent
+  });
+});
+
+
+
 injectFooter();
